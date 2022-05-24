7490f93b2c45cdabc3a6d46574e31ff6a4273ade
ScarlettHu <53459832+ScarlettHu@users.noreply.github.com>
2022-05-24 23:15:49 +1000

    Add model loading/batching/training/testing to COMP3221_FLClient.py.

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -8,6 +8,7 @@ import sys
 from torch.utils.data import DataLoader
 import matplotlib
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 def get_data(id=""):
     train_path = os.path.join("FLdata", "train", "mnist_train_" + str(id) + ".json")
@@ -35,4 +36,102 @@ port_client = int(sys.argv[2])
 opt_method = int(sys.argv[3])
 
 X_train, y_train, X_test, y_test, _, _ = get_data(client_id)
-print(X_train)
+
+train_data = [(x, y) for x, y in zip(X_train, y_train)]
+test_data = [(x, y) for x, y in zip(X_test, y_test)]
+
+loaders = {
+    'train' : torch.utils.data.DataLoader(train_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=0),
+    
+    'test'  : torch.utils.data.DataLoader(test_data, 
+                                          batch_size=100, 
+                                          shuffle=True, 
+                                          num_workers=0),
+}
+print(loaders)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output, x    # return x for visualization
+
+cnn = CNN()
+print(cnn)
+
+loss_func = nn.CrossEntropyLoss()   
+print(loss_func)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr = 0.01) 
+print(optimizer)
+
+num_epochs = 10
+def train(num_epochs, cnn, loaders):
+    
+    cnn.train()
+        
+    # Train the model
+    total_step = len(loaders['train'])
+        
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+            
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+            output = cnn(b_x)[0]               
+            loss = loss_func(output, b_y)
+            
+            # clear gradients for this training step   
+            optimizer.zero_grad()           
+            
+            # backpropagation, compute gradients 
+            loss.backward()    
+            # apply gradients             
+            optimizer.step()                
+            
+            if (i+1) % 10 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+train(num_epochs, cnn, loaders)
+
+def test():
+    # Test the model
+    cnn.eval()
+    with torch.no_grad():
+        test_counter = 0
+        accuracy = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = cnn(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy += (pred_y == labels).sum().item() / float(labels.size(0))
+            test_counter += 1
+        print('Test Accuracy of the model on the test images: %.2f' % (accuracy/test_counter))
+
+test()
