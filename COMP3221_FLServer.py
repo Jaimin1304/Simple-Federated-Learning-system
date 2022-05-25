dad4b2f27e5a2f03be9e0db49ad2f85707b10261
Jery1304 <jiaming2001_new@outlook.com>
2022-05-26 08:37:06 +1000

    minor server update

--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,12 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import os
-import json
-import copy
-from torch.utils.data import DataLoader
-import matplotlib
-import matplotlib.pyplot as plt
+import socket
+from time import sleep
+from sys import argv
+import _thread
+
+
+class MCLR(nn.Module):
+    def __init__(self):
+        super(MCLR, self).__init__()
+        # Create a linear transformation to the incoming data
+        # Input dimension: 784 (28 x 28), Output dimension: 10 (10 classes)
+        self.fc1 = nn.Linear(784, 10)
+
+    # Define how the model is going to be run, from input to output
+    def forward(self, x):
+        # Flattens input by reshaping it into a one-dimensional tensor. 
+        x = torch.flatten(x, 1)
+        # Apply linear transformation
+        x = self.fc1(x)
+        # Apply a softmax followed by a logarithm
+        output = F.log_softmax(x, dim=1)
+        return output
 
 def send_parameters(server_model, users):
     for user in users:
@@ -22,36 +38,45 @@ def aggregate_parameters(server_model, users, total_train_samples):
             server_param.data = server_param.data + user_param.data.clone() * user.train_samples / total_train_samples
     return server_model
 
-def evaluate(user):
+def evaluate(users):
     total_accurancy = 0
     for user in users:
         total_accurancy += user.test()
     return total_accurancy/len(users)
 
-# Init parameters 
-num_user = 5
+def server_handler(c, addr):
+    data_rev = c.recv(2048)
+    data_str = data_rev.decode('utf-8')
+    # 
+
+# Init parameters
+port_server = argv[1]
+sub_client = argv[2]
+IP = '127.0.0.1'
 users = []
 server_model = MCLR()
-batch_size = 20
 learning_rate = 0.01
 num_glob_iters = 100 # No. of global rounds
+curr_round = 0
 
-# TODO:  Create a federate learning network with 5 clients and append it to users list.
-total_train_samples = 0
-for i in range(1,num_user+1):
-    user = UserAVG(i, server_model, learning_rate, batch_size)
-    users.append(user)
-    total_train_samples += user.train_samples
-    send_parameters(server_model, users)
+# wait for the first handshake
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((IP, port_server)) # Bind to the port
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        _thread.start_new_thread(server_handler,(c, addr))
+        if curr_round >= num_glob_iters:
+            break
+    s.close()
+
 
 # Runing FedAvg
 loss = []
 acc = []
 
 for glob_iter in range(num_glob_iters):
-    
-    
-    # TODO: Broadcast global model to all clients
+    # Broadcast global model to all clients
     send_parameters(server_model,users)
     
     # Evaluate the global model across all clients
